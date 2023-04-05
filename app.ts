ab3c7329cb9615481c92c92144144091fbf625b6
syeef <syeefislam@yahoo.com>
2023-04-05 07:53:10 +0530

    typescript object and array

--- app.ts
+++ app.ts
@@ -2,9 +2,21 @@ const num1Element = document.getElementById('num1') as HTMLInputElement;
 const num2Element = document.getElementById('num2') as HTMLInputElement;
 const buttonElement = document.querySelector('button')!;
 
+const numResults:number[] = [];
+const textResults:string[] = [];
 
-function add(num1:number,num2:number){
+
+function add(num1:number|string,num2:number|string){
+   if(typeof num1 === 'number' && typeof num2 === 'number'){
     return num1 + num2;
+   }else if(typeof num1 === 'string' && typeof num2 === 'string'){
+    return num1 + '' + num2;
+   }
+   return +num1 + +num2
+}
+
+function printResult(resultObj:{val:number;timestamp:Date}){
+    console.log(resultObj.val);
 }
 
 
@@ -12,7 +24,12 @@ function add(num1:number,num2:number){
         const num1 = num1Element.value;
         const num2 = num2Element.value;
         const result = add(+num1,+num2);
-        console.log(result);
+        numResults.push(result as number);
+        const stringResult = add(num1,num2);
+        textResults.push(stringResult as string)
+        printResult({val:result as number,timestamp:new Date()});
+        console.log(numResults,textResults);
+        
     });
 
 
