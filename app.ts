076ebea1a21e6de6cfcb3aed931b8663a8962916
syeef <syeefislam@yahoo.com>
2023-04-05 10:27:24 +0530

    interface

--- app.ts
+++ app.ts
@@ -5,8 +5,18 @@ const buttonElement = document.querySelector('button')!;
 const numResults:number[] = [];
 const textResults:string[] = [];
 
+type NumOrString = number | string;
+type Rseult = {val:number;timestamp:Date};
 
-function add(num1:number|string,num2:number|string){
+
+interface ResultObj{
+    val:number;
+    timestamp:Date;
+}
+
+
+
+function add(num1:NumOrString,num2:NumOrString){
    if(typeof num1 === 'number' && typeof num2 === 'number'){
     return num1 + num2;
    }else if(typeof num1 === 'string' && typeof num2 === 'string'){
@@ -15,7 +25,7 @@ function add(num1:number|string,num2:number|string){
    return +num1 + +num2
 }
 
-function printResult(resultObj:{val:number;timestamp:Date}){
+function printResult(resultObj:ResultObj){
     console.log(resultObj.val);
 }
 
